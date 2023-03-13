09e961dbbf72cc8b9d7e5fa2b4a6cb37f86fb4e8
that-ambuj <ambujs89@gmail.com>
2023-03-13 22:35:24 +0530

    add basic_boot integration test module

---
+++ tests/basic_boot.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rusty_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rusty_os::test_panic_handler(info)
+}
+
+use rusty_os::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
