bc2a2e28d594abbc6833008580c380b0a6496b6e
that-ambuj <ambujs89@gmail.com>
2023-03-13 22:01:34 +0530

    add and implement Testable Trait

--- src/main.rs
+++ src/main.rs
@@ -52,10 +52,10 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test()
+        test.run()
     }
 
     exit_qemu(QemuExitCode::Success);
@@ -63,7 +63,20 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_println!("Trivial assertion");
-    assert_eq!(0, 1);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
 }
